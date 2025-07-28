b558491993dec12a41ac117950252bff6d1a2bca
hassaan <hassaanahhmed@gmail.com>
2025-07-28 14:55:37 +0500

    initially adding project

---
+++ lib/gradient_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/style.dart';
+
+var startAlignment= Alignment.topLeft ;
+var endAlignment = Alignment.bottomRight ;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context){
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors:  [ Colors.deepPurple, Colors.black45 ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+          child: style("Hello fans  "),
+
+      ),
+    );
+  }
+}
---
+++ lib/style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class style extends StatelessWidget{
+  const style(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(context) {
+
+    return Text (text,
+      style: TextStyle(
+        color: Colors.yellow,
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+}
